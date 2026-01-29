be085a11406bfb99d2e23577e50807641bb869d1
ivan55228899-art <ivan55228899@gmail.com>
2026-01-29 13:28:31 +0800

    server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const line = require('@line/bot-sdk');
 
@@ -9,18 +8,25 @@ const config = {
 
 const app = express();
 
-const handleCallback = (req, res) => {
+app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
-      console.error('Error handling events:', err);
+      console.error(err);
       res.status(500).end();
     });
-};
+});
 
-app.post('/callback', line.middleware(config), handleCallback);
-app.post('/', line.middleware(config), handleCallback);
+app.post('/', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
 
 app.get('/', (req, res) => {
   res.send('LINE Bot is running!');
@@ -30,15 +36,11 @@ function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
-
-  const client = new line.Client(config);
-  return client.replyMessage(event.replyToken, {
+  return new line.Client(config).replyMessage(event.replyToken, {
     type: 'text',
     text: event.message.text
   });
 }
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+app.listen(port, () => console.log(`Listening on ${port}`));
